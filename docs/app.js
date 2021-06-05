a6609ac383e4757b020556991d9df56559d0f095
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-05 14:11:21 -0400

    Add files via upload

---
+++ docs/app.js
@@ -0,0 +1,29 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// Display the array in console
+console.log("UFO Data:");
+console.log(tableData);
+
+// Arrays to hold movie scores
+var dateTime = [];
+var city = [];
+var state = [];
+var country = [];
+var shape = [];
+var comment = [];
+
+// Use a for loop to iterate through the movie scores
+for (var i = 0; i < tableData.length; i++) {
+
+  // Add each score to the ratings count
+  dateTime.push(tableData[i].slice(0));
+  city.push(tableData[i].slice(1));
+  state.push(tableData[i].slice(2));
+  country.push(tableData[i].slice(3));
+  shape.push(tableData[i].slice(4));
+  comment.push(tableData[i].slice(5));
+
+}
+
