108d288c153244e4b3036ac4da29ca433642d07f
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-07 19:27:37 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -7,46 +7,25 @@ var country = [];
 var shape = [];
 var comment = [];
 
-data.forEach((data) => {
+data.forEach(function(ufoSighting) {
+  console.log(ufoSighting);
+  });
+  
+data.forEach(function(ufoSighting) {
+  console.log(ufoSighting);
+  var row = tbody.append("tr");
 
-  Object.entries(data).forEach(([key, value]) => {
-
-    if (key === "datetime") {
-      dateTime.push(value);
-    }
-    else if (key === "city") {
-      city.push(value);
-    }
-    else if (key === "state") {
-      state.push(value);
-    }
-    else if (key === "country") {
-      country.push(value);
-    }
-    else if (key === "shape") {
-      shape.push(value);
-    }
-    else {
-      comment.push(value);
-    }
-   });
+  Object.entries(ufoSighting).forEach(function([key, value]) {
+  console.log(key, value);
+  var cell = row.append("td");
+  cell.text(value);
+  });
 });
-
-console.log(dateTime);
-console.log(city);
-console.log(state);
-console.log(country);
-console.log(shape);
-console.log(comment);
-
+  
 function searchMatch(match) {
-  // return player.madeTeam == true;
-  // A more concise way to express a boolean conditional
   return match.datetime;
 }
 
-// Call the custom function with filter()
 var dateEntered = data.filter(searchMatch);
 
-// Display the results
 console.log(dateEntered);
