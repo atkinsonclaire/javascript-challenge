670d2f0a452adcab8e0ba5c3fae9561e79e0cde6
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-07 20:51:56 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -7,31 +7,6 @@ var country = [];
 var shape = [];
 var comment = [];
 
-//data.forEach((data) => {
-
-  //Object.entries(data).forEach(([key, value]) => {
-
-    //if (key === "datetime") {
-      //dateTime.push(value);
-    //}
-    //else if (key === "city") {
-      //city.push(value);
-    //}
-    //else if (key === "state") {
-      //state.push(value);
-    //}
-    //else if (key === "country") {
-      //country.push(value);
-    //}
-    //else if (key === "shape") {
-      //shape.push(value);
-    //}
-    //else {
-      //comment.push(value);
-    //}
-   //});
-//});
-
 data.forEach(function(ufoSighting) {
   console.log(ufoSighting);
   var row = tbody.append("tr");
