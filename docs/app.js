093cd555c54f6851e700d53ead4a1676743df5b4
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-07 21:26:56 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -12,15 +12,16 @@ data.forEach(function(ufoSighting) {
 });
   
 var ufo = data;
-var button = d3.select("#button");
-
+var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
 function runEnter() {
-  
+
+  d3.event.preventDefault();
+
   var inputElement = d3.select("#datetime");
 
   var inputValue = inputElement.property("value");
