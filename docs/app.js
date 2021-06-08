18b783ab29580bf752bc40590480ce698189e4b7
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-08 16:14:05 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -16,7 +16,7 @@ var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+form.on("submit", runEnter);
 
 function runEnter() {
 
